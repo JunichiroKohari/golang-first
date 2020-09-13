c1c298df82e35c7eec3196d0c1110bee06d69031
JunichiroKohari <junichiro@kohari.jp>
2020-09-13 21:10:44 +0900

    print3種

--- main.go
+++ main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 // mainは最初に呼び出される関数
 func main() {
-	fmt.Printf("火星の表面で、私の体重は、")
-	fmt.Print(149.0 * 0.3783)
+	fmt.Print("火星の表面で、私の体重は、")
+	fmt.Print(53.0 * 0.3783)
+	fmt.Print("ポンド、年齢は、")
+	fmt.Print(24 * 365 / 687)
+	fmt.Println("歳になるでしょう。")
+	fmt.Println("printlnは改行")
+	fmt.Printf("火星の表面で、私の体重は、%v ポンド", 149.0 * 0.3783)
+	fmt.Printf("年齢は、%v 歳になるでしょう。\n", 41 * 365 / 687)
+	fmt.Printf("私の体重は、%vの表面で%vポンドです\n", "Earth", 149.0)
+	fmt.Printf("%-15v $%4v\n", "SpaceX", 94)
+	fmt.Printf("%-15v $%4v\n", "Virgin Galactic", 100)
 }
